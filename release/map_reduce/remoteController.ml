3aafad31209ecbe8688d1d58beeb21bc81038152
mingxx <mx62@cornell.edu>
2014-04-13 13:05:47 -0400

    change value in hash table to ref

--- release/map_reduce/remoteController.ml
+++ release/map_reduce/remoteController.ml
@@ -3,7 +3,8 @@ open Async.Std
 let workerList =  Hashtbl.create 8
 
 let init addrs =
-  List.iter (fun addr -> (Hashtbl.add) workerList (fst addr) (snd addr)) addrs
+  List.iter 
+  (fun addr -> (Hashtbl.add) workerList (fst addr) (ref (snd addr))) addrs
 
 module Make (Job : MapReduce.Job) = struct
   let map_reduce inputs =
