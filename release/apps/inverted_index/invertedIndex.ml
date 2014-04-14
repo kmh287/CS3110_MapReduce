fd26ea2ad857e074ffb478031144a7be610b734c
kmh287 <kmh287@cornell.edu>
2014-04-14 20:17:09 +0000

    Added a first atempt for the inverted_index app. 4:18 4-14-14

--- release/apps/inverted_index/invertedIndex.ml
+++ release/apps/inverted_index/invertedIndex.ml
@@ -8,18 +8,36 @@ type filename = string
 (******************************************************************************)
 
 module Job = struct
-  type input
-  type key
-  type inter
-  type output
+  type input = filename
+  type key = string (*A particular word*)
+  type inter = filename 
+  type output = string * string list
 
   let name = "index.job"
+  
+  module WS = Set.Make(String) 
+
+  (*Turn a string into a list of words in the string*) 
+   let rec separate acc s = 
+    if s = "" then acc else 
+      let trimmeds = String.trim s in (*Remove leading and trailing whitespace*)
+      let length = String.length trimmeds in 
+      let indexOfSpace =  try String.index trimmeds ' '
+                          with Not_found -> length-1 in
+      let beforeSpace = if indexOfSpace = length -1 then String.sub s 0 (indexOfSpace+1)
+                                                    else String.sub s 0 (indexOfSpace) in 
+      let afterSpace = String.sub s (indexOfSpace+1) (length - indexOfSpace -1) in 
+      separate (beforeSpace::acc) afterSpace ;;
 
   let map input : (key * inter) list Deferred.t =
-    failwith "I'm stepping through the door / And I'm floating in a most peculiar way / And the stars look very different today"
+    let fileName = input in 
+    let wordList = separate [] (Reader.file_contents input) in
+    let wordSet = List.fold_left (fun acc ele -> acc.add ele) WS.empty wordList in 
+    return  (List.map (wordSet.elements) (fun x -> (x,fileName)) )
+    
 
   let reduce (key, inters) : output Deferred.t =
-    failwith "Here am I floating round my tin can / Far above the Moon / Planet Earth is blue / And there's nothing I can do."
+    inters 
 end
 
 (* register the job *)
@@ -60,8 +78,13 @@ module App  = struct
 
     (** The input should be a single file name.  The named file should contain
         a list of files to index. *)
-    let main args =
-      failwith "Can you hear me, Major Zardoz? Can you hear me, Major Zardoz? Can you hear me, Major Zardoz?"
+    let main args = 
+      if args = [] then failwith "No file provided" 
+      else if List.length args > 1 then failwith "Applied to too many arguments"
+      else Reader.read_lines args
+        >>= MR.map_reduce
+        >>|output  
+      
   end
 end
 
