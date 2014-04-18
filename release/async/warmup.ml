55884e5c2dbde4544704e8df82516abfa77e832c
mingxx <mx62@cornell.edu>
2014-04-17 20:22:53 -0400

    change fork

--- release/async/warmup.ml
+++ release/async/warmup.ml
@@ -1,8 +1,7 @@
+open Async.Std
 
 let fork d f1 f2 =
-  d >>= f1;
-  d >>= f2;
-  ()
+  ignore (d >>= (fun x -> Deferred.both (f1 x) (f2 x)))
 
 let deferred_map l f =
   let tempList = List.map 
@@ -15,3 +14,4 @@ let deferred_map l f =
       | hd::tl -> (hd) >>= (fun x -> helper tl (acc@[x]))
   in
   helper tempList [] 
+
